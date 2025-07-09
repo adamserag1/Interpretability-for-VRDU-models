41ab0b78c458f50c7914677ec40c70252f09337a
adamserag1 <a.serag2@newcastle.ac.uk>
2025-07-09 18:01:27 +0100

    fix

--- Classification_Explain/lime.py
+++ Classification_Explain/lime.py
@@ -49,10 +49,10 @@ class LimeTextExplainer(BaseLimeExplainer):
         def fn(z_bin_list):
             perturbed = []
             for z in z_bin_list:
-                words = [w if z_i else self.mask_token for w, z_i in zip(sample["words"], z)]
+                words = [w if z_i else self.mask_token for w, z_i in zip(sample.words, z)]
                # boxes = [b if z_i else [0,0,0,0] for b, z_i in zip(sample.bboxes, z)] # change to height width of image
-                boxes = sample["bboxes"]
-                perturbed.append(DocSample(sample["image"], words, boxes))
+                boxes = sample.bboxes
+                perturbed.append(DocSample(sample.image, words, boxes))
             print("MADE PREDICT")
             return self._batched_predict(perturbed)
         return fn
