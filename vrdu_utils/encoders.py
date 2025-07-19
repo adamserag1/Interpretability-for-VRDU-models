9ebdc497784de9917972cde9d7898b3bf319929b
adamserag1 <a.serag2@newcastle.ac.uk>
2025-07-19 18:15:47 +0100

    Added sample finder script, added bros encoder for ner

--- vrdu_utils/encoders.py
+++ vrdu_utils/encoders.py
@@ -47,8 +47,8 @@ def make_layoutlmv3_encoder(processor, ner = False, max_length: int = 128):
 
     return encode
 
-# TODO: Allow NER encoder
-def make_bros_encoder_cls(tokenizer, max_length = 512):
+
+def make_bros_encoder(tokenizer, ner = False, max_length = 512):
     """
     BROS encoder for document classification.
     """
@@ -63,15 +63,39 @@ def make_bros_encoder_cls(tokenizer, max_length = 512):
             max_length=max_length,
             return_tensors="pt",
         )
-        aligned_boxes = []
-        for idx, s in enumerate(samples):
-            w, h = s.image.size
-            norm = [normalize_bbox(b, w, h) for b in s.bboxes]
-            word_ids = enc.word_ids(batch_index=idx)
-            aligned_boxes.append(
-                [norm[wid] if wid is not None else [0,0,0,0] for wid in word_ids]
-            )
-        enc["bbox"] = torch.tensor(aligned_boxes, dtype=torch.long)
+        if ner:
+            batch_normalized_bboxes, encoded_labels = [], []
+            for idx, (bboxes, img_path, labels) in enumerate(
+                    zip(samples["bboxes"], samples["image_path"], samples["ner_tags"])):
+                width, height = Image.open(img_path).size
+                normalized_bboxes = [normalize_bbox(bbox, width, height) for bbox in bboxes]
+
+                # Align boxes to sub words
+                aligned_boxes, aligned_labels = [], []
+                for word_id in enc.word_ids(batch_index=idx):
+                    if word_id is None:
+                        aligned_boxes.append([0, 0, 0, 0])
+                        aligned_labels.append(-100)
+                    else:
+                        aligned_boxes.append(normalized_bboxes[word_id])
+                        aligned_labels.append(labels[word_id])
+
+                batch_normalized_bboxes.append(aligned_boxes)
+                encoded_labels.append(aligned_labels)
+
+            enc['bbox'] = batch_normalized_bboxes
+            enc['labels'] = encoded_labels
+        else:
+            aligned_boxes = []
+            for idx, s in enumerate(samples):
+                w, h = s.image.size
+                norm = [normalize_bbox(b, w, h) for b in s.bboxes]
+                word_ids = enc.word_ids(batch_index=idx)
+                aligned_boxes.append(
+                    [norm[wid] if wid is not None else [0,0,0,0] for wid in word_ids]
+                )
+            enc["bbox"] = torch.tensor(aligned_boxes, dtype=torch.long)
+
         return _stack_on_decive(enc, device)
     return encode
 
