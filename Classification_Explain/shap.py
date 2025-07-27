938082cce249d087f5db085647109404e059822e
adamserag1 <a.serag2@newcastle.ac.uk>
2025-07-27 15:46:50 +0100

    debugging steps

--- Classification_Explain/shap.py
+++ Classification_Explain/shap.py
@@ -181,7 +181,7 @@ class SHAPTextExplainer(BaseShapExplainer):
 
         explainer = shap.Explainer(
             fn,
-            masker=background,
+            masker=tokenizer,
             algorithm=self.algorithm,
             # outputs=[sample.label],
             output_names=self.class_names,
