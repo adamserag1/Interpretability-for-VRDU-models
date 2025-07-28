7303e322fd2aa2626d429acac9a8f8fba91dd44d
adamserag1 <a.serag2@newcastle.ac.uk>
2025-07-28 17:41:16 +0100

    debugging steps

--- Classification_Explain/shap.py
+++ Classification_Explain/shap.py
@@ -302,7 +302,7 @@ class SHAPLayoutExplainer(BaseShapExplainer):
         n_tokens = len(sample.bboxes)
         # 1 = keep, 0 = mask
         data_row = np.ones(n_tokens, dtype=int)
-        baseline = np.zeros(n_tokens, dtype=int)  # what the masker inserts
+        baseline = np.zeros((1, n_tokens), dtype=int) # what the masker inserts
 
         masker = shap.maskers.Independent(baseline)
 
