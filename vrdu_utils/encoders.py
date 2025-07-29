c81eadcd18e72e105db6b318d477828579d01ba3
adamserag1 <a.serag2@newcastle.ac.uk>
2025-07-29 21:57:10 +0100

    added ner

--- vrdu_utils/encoders.py
+++ vrdu_utils/encoders.py
@@ -65,9 +65,9 @@ def make_bros_encoder(tokenizer, ner = False, max_length = 256):
         )
         if ner:
             batch_normalized_bboxes, encoded_labels = [], []
-            print(samples["bboxes"], samples["image_path"], samples["ner_tags"]) ## DEBUGGING
+            # print(samples.bboxes, samples.image_path, samples.ner_tags) ## DEBUGGING
             for idx, (bboxes, img_path, labels) in enumerate(
-                    zip(samples["bboxes"], samples["image_path"], samples["ner_tags"])):
+                    zip(samples.bboxes, samples.image_path, samples.ner_tags)):
                 width, height = Image.open(img_path).size
                 normalized_bboxes = [normalize_bbox(bbox, width, height) for bbox in bboxes]
 
