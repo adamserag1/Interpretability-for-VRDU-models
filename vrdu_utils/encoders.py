a3724fbd78e6dfcd25174431b15b8b14062ff5e0
adamserag1 <a.serag2@newcastle.ac.uk>
2025-07-30 15:26:18 +0100

    changed max length of encoders

--- vrdu_utils/encoders.py
+++ vrdu_utils/encoders.py
@@ -30,6 +30,7 @@ def make_layoutlmv3_encoder(processor, ner = False, max_length: int = 512):
                 max_length=max_length,
                 return_tensors="pt",
             )
+            return _stack_on_decive(enc, device)
         else:
             # for s in samples:
             #     w, h = s.image.size
@@ -45,11 +46,10 @@ def make_layoutlmv3_encoder(processor, ner = False, max_length: int = 512):
                 max_length=max_length,
                 return_tensors="pt",
             )
-            return enc.to(device)
+            return enc.to(device), _stack_on_decive(enc, device)
         # if "bbox" in enc:
         #     enc["bbox"] = enc["bbox"].clamp_(0, 1000)
 
-        return _stack_on_decive(enc, device)
 
     return encode
 
